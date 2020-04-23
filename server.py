286d1b7778e1bd15129db425b4a78637376f62f9
Parth Patel <32355079+pparth27743@users.noreply.github.com>
2020-04-23 14:19:56 +0530

    Problem of not able to call api multiple time resolved
    When we call FLASK API of the disease detection we are only able to call it once. Now that problem has been resolved by adding "K.clear_session()" after prediction.

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, Response, jsonify
 import jsonpickle
-import pickle
+import pickle, joblib
 import numpy as np
 import cv2   
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from keras.preprocessing.image import img_to_array
 from sklearn import preprocessing
+from keras import backend as K
 
 
 # app
@@ -17,13 +18,12 @@ app = Flask(__name__)
 
 def predict():
    
-    default_image_size = tuple((256, 256))
+
     file_object = open('cnn_model.pkl', 'rb')
-    model = pickle.load(file_object)
+    model = joblib.load(file_object)
 
     label_object = open('label_transform.pkl', 'rb')
-    label = pickle.load(label_object)
-    
+    label = joblib.load(label_object)
 
     r = request
 
@@ -32,7 +32,8 @@ def predict():
     
     # decode image
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)    
-    
+ 
+    default_image_size = tuple((256, 256))
     imar = cv2.resize(img, default_image_size)   
     imar = img_to_array(imar)
     npimagelist = np.array([imar], dtype=np.float16) / 255.0 
@@ -44,15 +45,28 @@ def predict():
 
     print(pred_result)
     
-
+    # for clearing up session so that flask api not get crashed when we call it multiple times
+    K.clear_session()
+    
     # build a response dict to send back to client
-    response = {'subject id': '{}'.format(pred_result)}
+    response = {'prediction': '{}'.format(pred_result)}
     # encode response using jsonpickle
     response_pickled = jsonpickle.encode(response)
 
     return Response(response=response_pickled, status=200, mimetype="application/json")
 
 
+@app.route('/', methods=['GET'])
+def getReq():
+   
+    response = {'msg': '{}'.format('Reply form get request')}
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+
 
 if __name__ == '__main__':
-    app.run(port = 5000, debug=True)
+    # app.run(port = 5000, debug=True)
+    app.run(debug=False, host='0.0.0.0', port = 5000)
+
+
