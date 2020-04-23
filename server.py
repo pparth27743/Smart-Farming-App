50e8df78a3bd14abb47f1ba0d496a41bc90e279f
Parth Patel <32355079+pparth27743@users.noreply.github.com>
2020-04-23 15:53:02 +0530

    added try and except

--- server.py
+++ server.py
@@ -17,53 +17,49 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 
 def predict():
-   
+    try:
+        file_object = open('cnn_model.pkl', 'rb')
+        model = joblib.load(file_object)
 
-    file_object = open('cnn_model.pkl', 'rb')
-    model = joblib.load(file_object)
+        label_object = open('label_transform.pkl', 'rb')
+        label = joblib.load(label_object)
 
-    label_object = open('label_transform.pkl', 'rb')
-    label = joblib.load(label_object)
+        r = request
 
-    r = request
-
-    # convert string of image data to uint8
-    nparr = np.fromstring(r.data, np.uint8)
+        # convert string of image data to uint8
+        nparr = np.fromstring(r.data, np.uint8)
+        
+        # decode image
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)    
     
-    # decode image
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)    
- 
-    default_image_size = tuple((256, 256))
-    imar = cv2.resize(img, default_image_size)   
-    imar = img_to_array(imar)
-    npimagelist = np.array([imar], dtype=np.float16) / 255.0 
+        default_image_size = tuple((256, 256))
+        imar = cv2.resize(img, default_image_size)   
+        imar = img_to_array(imar)
+        npimagelist = np.array([imar], dtype=np.float16) / 255.0 
 
 
-    predictResult = model.predict(npimagelist)
-    result = np.argmax(predictResult, axis=1)
-    pred_result = label.classes_[result[0]]
+        predictResult = model.predict(npimagelist)
+        result = np.argmax(predictResult, axis=1)
+        pred_result = label.classes_[result[0]]
 
-    print(pred_result)
+        print(pred_result)
+        
+        # for clearing up session so that flask api not get crashed when we call it multiple times
+        K.clear_session()
+        
+        # build a response dict to send back to client
+        response = {'prediction': '{}'.format(pred_result)}
+        # encode response using jsonpickle
+        response_pickled = jsonpickle.encode(response)
+        return Response(response=response_pickled, status=200, mimetype="application/json")
+
+    except Exception as e:
+        response = {'error': '{}'.format(e)}
+        response_pickled = jsonpickle.encode(response)
+        return Response(response=response_pickled, status=400, mimetype="application/json")
     
-    # for clearing up session so that flask api not get crashed when we call it multiple times
-    K.clear_session()
-    
-    # build a response dict to send back to client
-    response = {'prediction': '{}'.format(pred_result)}
-    # encode response using jsonpickle
-    response_pickled = jsonpickle.encode(response)
 
-    return Response(response=response_pickled, status=200, mimetype="application/json")
-
-
-@app.route('/', methods=['GET'])
-def getReq():
-   
-    response = {'msg': '{}'.format('Reply form get request')}
-    response_pickled = jsonpickle.encode(response)
-    return Response(response=response_pickled, status=200, mimetype="application/json")
-
-
+        
 
 if __name__ == '__main__':
     # app.run(port = 5000, debug=True)
